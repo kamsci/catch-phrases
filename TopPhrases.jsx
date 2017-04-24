d885768ca981374f71f13597b859aca486ef98d6
Krista Calderon <kamsci@gmail.com>
2017-04-23 22:54:49 -0700

    Renamed checkToAddPhrase to addedPhrase and refactored to pass test: should replace phrase with lowest count as obj in array

--- TopPhrases.jsx
+++ TopPhrases.jsx
@@ -2,7 +2,8 @@
   Assumptions:
   - The priority with phrases that have the same length is unknown
     - Therefore, in order to preserve data, if there is a tie in top ten counts, all phrases in the tied count will be returned
-    - Yes, this means >10 phrases can be returned
+    - This means >10 phrases can be returned
+  - Currently, this class is only used to add new phrases, not to remove
 */
 
 class TopPhrases {
@@ -12,30 +13,79 @@ class TopPhrases {
     this.phrases = {};
   }
 
-  checkToAddPhrase(phrase, count) {
+  addedPhrase(phrase, count) {
     if(phrase === "" || phrase === undefined
       || count < 1 || count === undefined) {
         return false;
       }
-    
-    // If phrases object has not reached max count capacity, add new phrase without checking existing counts
+
+    let didUpdate = false;
+    // Quantity of phrases count has not reached maxPhraseCounts
     if(this.maxPhraseCounts > Object.keys(this.phrases).length) {
+      // If count already exist, add phrase to array, if not add new key
       if(this.phrases.hasOwnProperty(count)) {
         this.phrases[count].push(phrase);
       } else {
         this.phrases[count] = [phrase];
       }
-      this.totalPhrases++;
-      return true;
+      // Update total phrases
+      this.totalPhrases++; 
+      console.log("Total: ", this.totalPhrases);
+      didUpdate = true;
+    } 
+    // Quantity of phrases has reached maxPhraseCounts, replace a key if count > minimum key
+    else {
+      didUpdate = this.tryReplacePhrase(phrase, count, this.totalPhrases);
     }
-      
-    let updated = false;
+    console.log("CheckDidUpdate", didUpdate);
+    return didUpdate;
+  }
 
+  tryReplacePhrase(phrase, count, totalPhrases) {
+    let updated = false;
+    let min = this.findMinCount();
+    console.log("TotalInReplace: ", totalPhrases);
+
+    // Return without updating if count is less than current min
+    if (count <  min) { return updated; }
+
+    let newPhraseState = {};
+
+    // Create newPhraseState to replace this.phrases
+    for(var countKey in this.phrases) {
+      // Find minimum key and replace with new key,value (count, phrase)
+      if(countKey === min) {
+        newPhraseState[count] = [phrase];
+        updated = true;
+
+        // Update totalPhrases
+        this.updatePhraseCount(totalPhrases, this.phrases[countKey].length);     
+      } 
+      else {
+        newPhraseState[countKey] = this.phrases[countKey];
+      }
+    }
+    this.phrases = newPhraseState;
+    console.log("newTop", this)
+    console.log("newTotal:", this.totalPhrases)
     return updated;
   }
 
-  replacePhrase(phrase, count) {
+  findMinCount() {
+    if(this.totalPhrases = 0) { return 0; }
 
+    let min = Number.MAX_SAFE_INTEGER;
+    for(var countKey in this.phrases) {
+      if(countKey < min) {
+        min = countKey;
+      }
+    }
+    return min;
+  }
+
+  updatePhraseCount(total, remove) {
+    console.log("TotalUpdate: ", total);
+    this.totalPhrases = (total - remove) + 1;
   }
 };
 
--- test/TopPhrases.test.js
+++ test/TopPhrases.test.js
@@ -39,8 +39,8 @@ describe('Check to add Phrase: ', () => {
     let count2;
 
     let actualTopTenObj = new TopPhrases();
-    let added = actualTopTenObj.checkToAddPhrase(phrase, count);
-    let added2 = actualTopTenObj.checkToAddPhrase(phrase2, count2);
+    let added = actualTopTenObj.addedPhrase(phrase, count);
+    let added2 = actualTopTenObj.addedPhrase(phrase2, count2);
 
     expect(added).to.be.false;
     expect(added2).to.be.false;
@@ -60,14 +60,14 @@ describe('Check to add Phrase: ', () => {
       let count = 1;
       
       let actualTopTenObj = new TopPhrases(10);
-      let added = actualTopTenObj.checkToAddPhrase(phrase, count);
+      let added = actualTopTenObj.addedPhrase(phrase, count);
       
       expect(actualTopTenObj).to.deep.equal(expectedTopTenObj);
       expect(added).to.be.true;
     });
   });
 
-  describe('If TopPhrases obj is not empty but not at max', () => {
+  describe('If TopPhrases obj is not empty but not at maxPhraseCounts', () => {
     describe('and new phrase count is unique', () => {
       it('should add the count key with phrase as value array and return true', () => {
         let expectedTopTenObj = {
@@ -85,8 +85,8 @@ describe('Check to add Phrase: ', () => {
         let count2 = 2;
         
         let actualTopTenObj = new TopPhrases(10);
-        let added = actualTopTenObj.checkToAddPhrase(phrase, count);
-        let added2 = actualTopTenObj.checkToAddPhrase(phrase2, count2);
+        let added = actualTopTenObj.addedPhrase(phrase, count);
+        let added2 = actualTopTenObj.addedPhrase(phrase2, count2);
 
         expect(actualTopTenObj).to.deep.equal(expectedTopTenObj);
         expect(added).to.be.true;
@@ -114,9 +114,9 @@ describe('Check to add Phrase: ', () => {
         let count3 = 2;
         
         let actualTopTenObj = new TopPhrases(10);
-        let added = actualTopTenObj.checkToAddPhrase(phrase, count);
-        let added2 = actualTopTenObj.checkToAddPhrase(phrase2, count2);
-        let added3 = actualTopTenObj.checkToAddPhrase(phrase3, count3);
+        let added = actualTopTenObj.addedPhrase(phrase, count);
+        let added2 = actualTopTenObj.addedPhrase(phrase2, count2);
+        let added3 = actualTopTenObj.addedPhrase(phrase3, count3);
 
         expect(actualTopTenObj).to.deep.equal(expectedTopTenObj);
         expect(added).to.be.true;
@@ -126,37 +126,41 @@ describe('Check to add Phrase: ', () => {
     });
   });
 
-  describe('If TopPhrases obj is full and new phrase count unique and higher than an exisitng count', () => {
-    it('should replace phrase with lowest count as obj in array and return true', () => {
-      let expectedTopTenObj = {
-        maxPhraseCounts: 2,
-        totalPhrases: 3,
-        phrases: { 
-          "2": ["not one but"],
-          "3": ["two well-thought-out sentences"] 
+  describe('If TopPhrases obj is at maxPhraseCounts', () => {
+    describe('and new phrase count is unique and higher than both exisitng counts', () => {
+      it('should replace phrase with lowest count as obj in array and return true', () => {
+        let expectedTopTenObj = {
+          maxPhraseCounts: 2,
+          totalPhrases: 2,
+          phrases: {         
+            "2": ["two well-thought-out sentences"], 
+            "3": ["not one but"],
+          }
         }
-      }
-      let phrase = "it has two";
-      let count = 1;
+        let phrase = "it has two";
+        let count = 1;
 
-      let phrase2 = "two well-thought-out sentences"
-      let count2 = 3;
+        let phrase2 = "two well-thought-out sentences"
+        let count2 = 2;
 
-      let phrase3 = "not one but";
-      let count3 = 2;
-      
-      let actualTopTenObj = new TopPhrases(2); // created obj with max of 2 phrases
-      let added = actualTopTenObj.checkToAddPhrase(phrase, count);
-      let added2 = actualTopTenObj.checkToAddPhrase(phrase2, count2);
-      let added3 = actualTopTenObj.checkToAddPhrase(phrase3, count3);
+        let phrase3 = "not one but";
+        let count3 = 3;
+        
+        let actualTopTenObj = new TopPhrases(2); // created obj with max of 2 phrases
+        let added = actualTopTenObj.addedPhrase(phrase, count);
+        let added2 = actualTopTenObj.addedPhrase(phrase2, count2);
+        let added3 = actualTopTenObj.addedPhrase(phrase3, count3);
 
-      expect(actualTopTenObj).to.deep.equal(expectedTopTenObj);
-      expect(added3).to.be.true;
-    });
+        expect(actualTopTenObj).to.deep.equal(expectedTopTenObj);
+        expect(added).to.be.true;
+        expect(added2).to.be.true;
+        expect(added3).to.be.true;
+      });
 
   //   it('should handle a tie with no more slots left', () => {
           
   //   });
+    });
   });
 
 });
