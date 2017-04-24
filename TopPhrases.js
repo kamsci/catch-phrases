3dba1344ffaf76200680ce77ed9f0fc8dcd7056d
Krista Calderon <kamsci@gmail.com>
2017-04-24 16:56:29 -0700

    Added 2 unit tests for TopPhrases, passing; Refacotring TopPhrases to keep track of a sorted phraseArray

--- TopPhrases.js
+++ TopPhrases.js
@@ -1,27 +1,31 @@
 class TopPhrases {
   constructor(phraseObj){
     this.phraseObj = phraseObj;
+    this.sortedPhraseArray;
     this.topPhrasesObj;
 
     this.determineTopPhrases();
   }
 
   determineTopPhrases() {
-    let sortedCountArray = this.turnPhraseObjIntoSortedArray();
-    let topCountsArray = this.getHighestCountsArray(sortedCountArray);
-    let topPhrasesObj = this.getTopTenArrayOfPhrases(topCountsArray);
+    this.sortedPhraseArray = this.turnPhraseObjIntoSortedArray();
+    // let topCountsArray = this.getHighestCountsArray(sortedCountArray);
+    // let topPhrasesObj = this.getTopTenArrayOfPhrases(topCountsArray);
 
-    this.topPhrasesObj = topPhrasesObj;
+    // this.topPhrasesObj = topPhrasesObj;
   }
 
   turnPhraseObjIntoSortedArray() {
-    let countArray = [];
-    for(var phrase in this.phraseObj){
-      let count = this.phraseObj[phrase];
-      countArray.push(count);
+    if(this.phraseObj === undefined || this.phraseObj === {}) { return; }
+
+    let sortedPhraseArray = [];
+    for(var phrase in this.phraseObj) {
+      sortedPhraseArray.push([phrase, this.phraseObj[phrase]]);
     }
-    let sortedCountArray = countArray.sort();
-    return sortedCountArray;
+    sortedPhraseArray.sort(function(a, b) {
+      return b[1] - a[1];
+    });
+    return sortedPhraseArray;
   }
   
   getHighestCountsArray(sortedCountArray) {
@@ -50,9 +54,9 @@ class TopPhrases {
         }
       }
     }
-    // console.log("Before topPhrases", topPhrases)
+    console.log("Before topPhrases", topPhrases)
     let newTopPhrases = this.removeSubsets(topPhrases);
-    // console.log("New length", newTopPhrases.length);
+    console.log("New length", newTopPhrases.length);
     // TODO: Repeat get phrases to add up to 10
     return newTopPhrases;
   }
@@ -70,7 +74,7 @@ class TopPhrases {
         }
       }
     }
-    // console.log("After topPhrases", topPhrases)
+    console.log("After topPhrases", topPhrases)
     return topPhrases;
   }
 }
--- test/TopPhrases.test.js
+++ test/TopPhrases.test.js
@@ -1,13 +1,25 @@
 var expect = require('chai').expect;
 var TopPhrases = require('../TopPhrases.js');
+var expectedPhraseArrays = require('./expectedPhraseArrays.json');
+
 
 /* 
   Coming Soon! - Tests for TopPhrases class
 */
 describe('Instantiate new TopPhrases object,', () => {
-  it('should create a new object with defaults', () => {
+  it('should create a sortedPhraseArray when passed a phraseObj', () => {
+    let expectedResult = expectedPhraseArrays.sortedPhraseArray;
+    
+    let topPhrases = new TopPhrases(expectedPhraseArrays.phraseObj);
+    let actualResult = topPhrases.turnPhraseObjIntoSortedArray();
 
-
+    expect(actualResult).to.be.an('array');
+    expect(expectedResult).to.deep.equal(actualResult);
   });
+  it('should return undefined when no phraseObj', () => { 
+    let topPhrases = new TopPhrases();
+    let actualResult = topPhrases.turnPhraseObjIntoSortedArray();
 
+    expect(actualResult).to.be.undefined;
+  });
 });
